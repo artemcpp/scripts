f70b6023ca9285d977649ad5c8d0448d8f5e68b2
artemcpp <artemcpp@gmail.com>
2014-05-06 19:32:50 +0400

    ----

---
+++ get-cif.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import sys
+import pymatgen as pm
+import pymatgen.io.cifio as cif
+
+for id in sys.argv[1:]:
+	m = pm.MPRester("***REMOVED***")
+	struct = m.get_structure_by_material_id(id, False)
+	ciff = cif.CifWriter(struct)
+
+	ciff.write_file(id + '.cif')
